a3cc13c8b94af7ebd16fa98c22d767914d510f68
acabe10 <alejandrocabezab@gmail.com>
2019-03-04 17:46:30 +0100

    comentarios en liga.py

---
+++ liga.py
@@ -0,0 +1,27 @@
+from lxml import etree
+
+# Pedir número de jornada y año
+
+	# Abrir documento dependiendo de jornada y año
+
+	# Mostrar partidos, hora y canal de tv
+
+# Pedir número de jornada y año
+	# Abrir documento de esa jornada y año
+	# Contar goles de esa jornada
+
+# Pedir jornada, año y canal de TV
+	# Abrir documento de jornada y año
+	# Mostrar partidos de esa jornada en ese canal
+
+# Pedir temporada y arbitro
+	# Abrir documentos de temporada
+	# Mostrar partidos arbitrados por ese arbitro
+
+# Pedir una jornada
+	# Abrir documento de jornada
+	# Id mostrando partidos de esa jornada y al final mostrar si
+	# si se ha acertado con la quiniela	
+doc = etree.parse('prueba.xml')
+
+print(doc.xpath("//price/text()"))
