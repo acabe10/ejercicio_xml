83f8f6aeda3df46fc5689934c846aa4f504ca43b
acabe10 <alejandrocabezab@gmail.com>
2019-03-05 21:45:24 +0100

    preparacion de opciones

--- liga.py
+++ liga.py
@@ -2,17 +2,8 @@ from lxml import etree
 arbol = etree.parse('liga.xml')
 cont = 0
 # Pedir número de jornada y año
-
 	# Abrir documento dependiendo de jornada y año
-#eventos=arbol.xpath("//evento")
-#print(eventos)
-#for datos in eventos:
-#	print(datos.xpath("./fecha/text()")[cont])
-#	print(datos.xpath("./equipolocal/text()")[cont])
-#	print(datos.xpath("./equipovisitante/text()")[cont])
-#	print(datos.xpath("./tv/text()")[cont])
-#	cont = cont + 1
-#	print(cont)
+
 	# Mostrar partidos, hora y canal de tv
 def partidos(arbol):
 	fecha=arbol.xpath("//evento/fecha/text()")
@@ -21,8 +12,32 @@ def partidos(arbol):
 	canal=arbol.xpath("//evento/tv/text()")
 	return zip(fecha,local,visitante,canal)
 
-for a,e,i,o in partidos(arbol):
-    print ("Fecha y Hora:",a,e,"-",i,"   Canales:",o)
+while True:
+	print("1.Pedir jornada y año y mostrar partidos, hora y canal de TV")
+	print("2.Te cuenta el número de goles que se han marcado en una determinada jornada")
+	print("3.Pedir una jornada y canal de TV,y te dice qué partidos y a qué hora se emitirán en ese canal")
+	print("4.Pedir árbitro y temporada y decir cuantos partidos ha arbitrado")
+	print("5.Quiniela de jornada seleccionada")
+	print("0.Salir")
+	opcion=int(input("Elige opción: "))
+
+	if opcion == 0:
+		print()
+		print("Adiós!")
+		print()
+		break;
+
+	elif opcion == 1:
+		for fecha,local,visi,tv in partidos(arbol):
+		    print ("Fecha y hora:",fecha)
+		    print("Local: %s -- Visitante: %s" % (local,visi))
+		    print("Canal/es de TV: %s" % tv)
+		    print("="*40)
+
+	else:
+		print()
+		print("Error de opción")
+		print()
 
 # Pedir número de jornada y año
 	# Abrir documento de esa jornada y año
