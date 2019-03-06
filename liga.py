0f2a1c347444cec730792cee8bb5ff6cd45efc3e
acabe10 <alejandrocabezab@gmail.com>
2019-03-06 19:29:11 +0100

    preparacion ej4

--- liga.py
+++ liga.py
@@ -63,6 +63,98 @@ def canal(arbol):
 	fecha=arbol.xpath('//evento[tv="%s"]/fecha/text()'%tv_)
 	return zip(fecha,local,visi)
 
+def pedir_equipo():
+	while True:
+		print("Dime el equipo:")
+		print("1.R. Madrid")
+		print("2.Barcelona")
+		print("3.Eibar")
+		print("4.Leganés")
+		print("5.Levante")
+		print("6.Valencia")
+		print("7.R. Sociedad")
+		print("8.Atlético")
+		print("9.Girona")
+		print("10.Espanyol")
+		print("11.Sevilla")
+		print("12.Athletic")
+		print("13.Getafe")
+		print("14.Celta")
+		print("15.Alavés")
+		print("16.Las Palmas")
+		print("17.Málaga")
+		print("18.Villarreal")
+		print("19.Betis")
+		print("20.Deportivo")
+		print()
+		opcion=int(input("Elige opción: "))
+		print()
+
+		if opcion == 1:
+			equipo="R. Madrid"
+			return equipo
+		elif opcion == 2:
+			equipo="Barcelona"
+			return equipo
+		elif opcion == 3:
+			equipo="Eibar"
+			return equipo
+		elif opcion == 4:
+			equipo="Leganés"
+			return equipo
+		elif opcion == 5:
+			equipo="Levante"
+			return equipo
+		elif opcion == 6:
+			equipo="Valencia"
+			return equipo
+		elif opcion == 7:
+			equipo="R. Sociedad"
+			return equipo
+		elif opcion == 8:
+			equipo="Atlético"
+			return equipo
+		elif opcion == 9:
+			equipo="Girona"
+			return equipo
+		elif opcion == 10:
+			equipo="Espanyol"
+			return equipo
+		elif opcion == 11:
+			equipo="Sevilla"
+			return equipo
+		elif opcion == 12:
+			equipo="Athletic"
+			return equipo
+		elif opcion == 13:
+			equipo="Getafe"
+			return equipo
+		elif opcion == 14:
+			equipo="Celta"
+			return equipo
+		elif opcion == 15:
+			equipo="Alavés"
+			return equipo
+		elif opcion == 16:
+			equipo="Las Palmas"
+			return equipo
+		elif opcion == 17:
+			equipo="Málaga"
+			return equipo
+		elif opcion == 18:
+			equipo="Villarreal"
+			return equipo
+		elif opcion == 19:
+			equipo="Betis"
+			return equipo
+		elif opcion == 20:
+			equipo="Deportivo"
+			return equipo
+		else:
+			print()
+			print("Error de opción")
+			print()
+
 while True:
 	print("1.Pedir jornada y año y mostrar partidos, hora y canal de TV")
 	print("2.Te cuenta el número de goles que se han marcado en una determinada jornada")
@@ -109,6 +201,9 @@ while True:
 		    print("Local: %s -- Visitante: %s" % (local,visi))
 		    print("="*40)
 		print()
+
+	elif opcion == 4:
+		equipo=pedir_equipo()
 
 	else:
 		print()
